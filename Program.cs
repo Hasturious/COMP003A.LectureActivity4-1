878f918584a6c4c2d5e9996e0d64a53a4a9d083f
Jean Figeuroa <jbster559@gmail.com>
2025-02-03 18:12:31 -0800

    Student names display properly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using static System.Formats.Asn1.AsnWriter;
+﻿// Author: Jean Bryant Figueroa
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Calculating average grades using arrays and list in C#
+
+using static System.Formats.Asn1.AsnWriter;
 
 namespace COMP003A.LectureActivity4_1
 {
@@ -16,7 +21,7 @@ namespace COMP003A.LectureActivity4_1
                     Console.Write($"Enter grade for student {i + 1} (0-100): ");
                     if (int.TryParse(Console.ReadLine(), out grade)) //check if input is valid interger
                     {
-                        if (grade < 0 || grade > 100)
+                        if (grade >= 0 && grade <= 100)
                         {
                             grades[i] = grade;
                         }
@@ -45,6 +50,21 @@ namespace COMP003A.LectureActivity4_1
                 Console.WriteLine(grade);
             }
             Console.WriteLine($"Average Grade: {average:F2}");
+
+            List<string> studentNames = new List<string>();
+
+            //Collecting student names
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter the name for student {i + 1}: ");
+                studentNames.Add(Console.ReadLine()); //adds the name to the list
+            }
+
+            Console.WriteLine("\nStudent Names: ");
+            for (int i = 0; i < studentNames.Count; i++)
+            {
+                Console.WriteLine($"Student {i + 1}: {studentNames[i]}");
+            }
         }
     }
 }
