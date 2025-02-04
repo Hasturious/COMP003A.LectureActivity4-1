253bd0503ed1c01710f8ac0147ebcd57827c3717
Jean Bryant Figueroa <jbster559@gmail.com>
2025-02-03 16:26:48 -0800

    Added arrays

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace COMP003A.LectureActivity4_1
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace COMP003A.LectureActivity4_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] grades = new int[5];
+            string[] names = new string[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                int grade = -1
+                while (grade <0 || grade > 100)
+                {
+                Console.WriteLine
+                }
+                    Console.WriteLine("Grade " + ": " + grad)
+            }
         }
     }
 }
