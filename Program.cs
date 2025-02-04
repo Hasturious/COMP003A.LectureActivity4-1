5606dbcbea08392e06fb880fe1f3a0964b650eb7
Jean Bryant Figueroa <jbster559@gmail.com>
2025-02-03 16:56:45 -0800

    Added grade validator

--- Program.cs
+++ Program.cs
@@ -7,14 +7,28 @@ namespace COMP003A.LectureActivity4_1
         static void Main(string[] args)
         {
             int[] grades = new int[5];
-            string[] names = new string[5];
 
             for (int i = 0; i < grades.Length; i++)
             {
-                int grade = -1
+                int grade = -1;
                 while (grade <0 || grade > 100)
                 {
-                Console.WriteLine
+                    Console.Write($"Enter grade for student {i + 1} (0-100): ");
+                    if (int.TryParse(Console.ReadLine(), out grade)) //check if input is valid interger
+                    {
+                        if (grade < 0 || grade > 100)
+                        {
+                            grades[i] = grade;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Grade must be between 0 and 10");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input. Please enter a valid number");
+                    }
                 }
                     Console.WriteLine("Grade " + ": " + grad)
             }
